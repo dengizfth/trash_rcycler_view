1c9b120d0626f861167e20d0f83966e9cdbbbbe7
Fatih Dengiz <dengizfth@gmail.com>
2024-07-20 15:43:40 +0300

    Adapter.kt

---
+++ app/src/main/java/com/fatihden/recyclerview/Adapter.kt
@@ -0,0 +1,31 @@
+package com.fatihden.recyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.fatihden.recyclerview.databinding.ActivityMainBinding
+import com.fatihden.recyclerview.databinding.MainCardViewBinding
+
+class Adapter(val kahListe:ArrayList<SuperKahraman>) :
+    RecyclerView.Adapter<Adapter.ViewHolder>()  {
+    class ViewHolder(val binding: MainCardViewBinding):RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = MainCardViewBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return kahListe.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.tvRecyclerView.text = kahListe[position].isim
+    }
+
+
+
+
+}
