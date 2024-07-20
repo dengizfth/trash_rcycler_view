7b49d915b0be7e6f0d8482214b7949bf60827751
Fatih Dengiz <dengizfth@gmail.com>
2024-07-20 14:23:58 +0300

    obje ve listeler oluşturuldu

--- app/src/main/java/com/fatihden/recyclerview/MainActivity.kt
+++ app/src/main/java/com/fatihden/recyclerview/MainActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.fatihden.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+    private lateinit var superKahramanListe: ArrayList<SuperKahraman>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = _binding.root
+        setContentView(view)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val superman = SuperKahraman("Ironman", "Teknoloji", R.drawable.iron)
+        val hulk = SuperKahraman("Hulk", "Doktor", R.drawable.hulk_)
+        val docterStrage = SuperKahraman("Doktor Strage", "Doktor", R.drawable.docter)
+        val amerika = SuperKahraman("Amerika ", "Asker", R.drawable.amerika)
+
+
+        superKahramanListe = arrayListOf(superman, hulk, docterStrage, amerika)
+
     }
+
+
 }
